c65471949f625e54a12df97f98c68c8b698f13d9
xukecheng <cnxukecheng@gmail.com>
2023-02-23 11:15:54 +0800

    chore: new shunt

--- routes.js
+++ routes.js
@@ -14,40 +14,47 @@ router.route('/').get((req, res) => {
 router.route('/parser').get(async (req, res) => {
   let result = { message: 'No URL was provided' };
 
+
   if (req.query.url) {
     try {
       let browser;
+      let html;
+      let parserBody;
+
+      const contentType = req.query.contentType || 'html';
+      let headers = new Object();
+      if (typeof req.query.headers !== 'undefined') {
+        headers = JSON.parse(req.query.headers);
+      }
 
       if (config.browserless_url !== null && req.query.browserless) {
         console.log('Server in browserless');
         browser = await puppeteer.connect({
           browserWSEndpoint: config.browserless_url,
         });
+        // Fetch the HTML of the URL using Puppeteer
+        const page = await browser.newPage();
+        await page.setUserAgent(
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64"
+        );
+        await page.goto(req.query.url, { "timeout": "10000", "waitUntil": 'networkidle2', });
+        html = await page.content();
+        browser.close();
+        parserBody = {
+          html,
+          contentType,
+          headers,
+        };
       } else {
         console.log('Server in no browserless');
-        browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'], });
+        parserBody = {
+          contentType,
+          headers,
+        };
       }
-
-      // Fetch the HTML of the URL using Puppeteer
-      const page = await browser.newPage();
-      await page.setUserAgent(
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64"
-      );
-      await page.goto(req.query.url, { "timeout": "10000", "waitUntil": 'networkidle2', });
-      const html = await page.content();
-      browser.close();
 
       // Pass the HTML to the Mercury.parse function
-      const contentType = req.query.contentType || 'html';
-      let headers = new Object();
-      if (typeof req.query.headers !== 'undefined') {
-        headers = JSON.parse(req.query.headers);
-      }
-      result = await Parser.parse(req.query.url, {
-        html,
-        contentType,
-        headers,
-      });
+      result = await Parser.parse(req.query.url, parserBody);
 
     } catch (error) {
       result = { error: true, messages: error.message };
