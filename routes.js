6c2cebf7dc8a6c7082b9ee8cdd7695d086cde92d
xukecheng <cnxukecheng@gmail.com>
2023-02-24 09:51:29 +0800

    feat: add new query timeout

--- routes.js
+++ routes.js
@@ -37,7 +37,7 @@ router.route('/parser').get(async (req, res) => {
         await page.setUserAgent(
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64"
         );
-        await page.goto(req.query.url, { "timeout": "10000", "waitUntil": 'networkidle2', });
+        await page.goto(req.query.url, { "timeout": req.query.timeout || "10000", "waitUntil": 'networkidle2', });
         html = await page.content();
         browser.close();
         parserBody = {
